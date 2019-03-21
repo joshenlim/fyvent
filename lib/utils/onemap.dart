90636a5ed87d2b18e4960d76d38b6faeb4616493
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-03-21 23:19:20 +0800

    Improvements to OneMap API

---
+++ lib/utils/onemap.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List> getBankLocations(String bankName) async {
+  List listOfBanks = [];
+  final requestURL =
+      "https://developers.onemap.sg/commonapi/search?searchVal=" +
+          bankName +
+          "&returnGeom=Y&getAddrDetails=Y";
+
+  final response = await http.get(requestURL);
+  if (response.statusCode == 200) {
+    final responseJson = json.decode(response.body);
+    responseJson["results"].forEach((bank) {
+      listOfBanks.add({
+        "address": bank["BUILDING"] + ", " + bank["ROAD_NAME"],
+        "lat": bank["LATITUDE"],
+        "lng": bank["LONGITUDE"],
+      });
+    });
+  } else {
+    print("Error OneMap: " + response.statusCode.toString());
+  }
+  return listOfBanks;
+}
