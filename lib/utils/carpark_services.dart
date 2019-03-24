41481bfc86c7fbdffa0407d061678bd626af12e2
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-03-24 17:21:07 +0800

    Implement getCarparks from Datamall, and renamed util files related to apis

---
+++ lib/utils/carpark_services.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+final accountKey = "II2mAyMiRWy0o4L/jVwzzQ==";
+
+Future<List> getCarparkLocations() async {
+  List listOfCarparks = [];
+  final requestURL = "http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2";
+
+  final response = await http.get(requestURL,
+    headers: {
+      'AccountKey' : accountKey  
+    }
+  );
+  if (response.statusCode == 200) {
+    final responseJson = json.decode(response.body);
+    responseJson["value"].forEach((carpark) {
+      if (carpark["LotType"] == "C" || carpark["LotType"] == "M") {
+        List<String> latLng = carpark["Location"].split(" ");
+        listOfCarparks.add({
+          "address": carpark["Development"] + " (" + carpark["Area"] + ")",
+          "lat": latLng[0],
+          "lng": latLng[0],
+          "availableLots": carpark["AvailableLots"],
+        });
+      }
+    });
+  } else {
+    print("Error Datamall: " + response.statusCode.toString());
+  }
+  return listOfCarparks;
+}
