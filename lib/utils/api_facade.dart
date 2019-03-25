0654c008bfcb5ba3d25f3b031378d753ba390785
Unknown <c170127@e.ntu.edu.sg>
2019-03-25 21:21:39 +0800

    implemented api_facade
    -created facade "class" that calls that various APIs methods
    -shifted APIs to a their own folder

---
+++ lib/utils/api_facade.dart
@@ -0,0 +1,23 @@
+import 'package:fyvent/models/event.dart';
+import 'package:fyvent/utils/api/bank_api.dart' as bankAPI;
+import 'package:fyvent/utils/api/carpark_api.dart' as carparkAPI;
+import 'package:fyvent/utils/api/event_api.dart' as eventAPI;
+
+Future<List> getBankLocations(String bankName) async =>
+    bankAPI.getBankLocations(bankName);
+
+Future<List> getCarparkLocations() async =>
+    carparkAPI.getCarparkLocations();
+
+Future<List<Event>> getEvents(int qty) async =>
+    eventAPI.getEvents(qty);
+
+Future<List<Event>> searchEvents(String query) async =>
+    eventAPI.searchEvents(query);
+
+bool checkIfEventInFavourites(List favourites, int id) =>
+    eventAPI.checkIfEventInFavourites(favourites, id);
+
+bool checkIfEventInUserFavourites(List<Event> favourites, int id) =>
+    eventAPI.checkIfEventInUserFavourites(favourites, id);
+
