b3bb39158bad08d02b3f191a37d6f4d9a515ffca
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-04-04 14:07:36 +0800

    Refactor featured event card

--- lib/components/FeaturedEventCard.dart
+++ lib/components/FeaturedEventCard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fyvent/screens/event_detail_screen.dart';
+import 'package:fyvent/models/event.dart';
 
 class FeaturedEventCard extends StatelessWidget {
   final String imgUrl;
@@ -12,91 +14,126 @@ class FeaturedEventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
+
+    void _showEventDetail() {
+      Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (BuildContext context) => EventDetailScreen(event: new Event(
+          id: 9099,
+          name: 'Tis The Sea-sun',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit finibus tortor, et lobortis lorem porta hendrerit. Maecenas ut ex fringilla, ultrices nisi non, ornare nisi. Aenean quis pulvinar quam, eu viverra sapien. Suspendisse sed diam venenatis, interdum lectus et, dapibus purus.',
+          address: '3 Temasek Boulevard',
+          locationDesc: 'Suntec City - Event Hall 3',
+          datetimeStart: '2019-05-15 10:00:00',
+          datetimeEnd:  '2019-05-15 18:00:00',
+          datetimeRange: '15 April 2019 • 10.00 AM - 6.00 PM',
+          category: 'Travel',
+          imgUrl: imgUrl,
+          webUrl: 'https://sunteccity.com.sg/',
+          lat: 1.2941,
+          lng: 103.8578,
+          ticketPrices: [],
+        ))
+      ));
+    }
+
     return new Container(
       height: 300,
       width: width,
-      padding: const EdgeInsets.all(20.0),
-      decoration: new BoxDecoration(
-        image: new DecorationImage(
-          image: new NetworkImage(imgUrl),
-          fit: BoxFit.cover,
-          alignment: Alignment.center,
-        )
-      ),
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: new Stack(
         children: [
-          new Text(
-            title,
-            style: new TextStyle(
-              fontFamily: 'Greycliff',
-              color: Colors.white,
-              fontSize: 40,
-            )
-          ),
-          new Text(
-            description,
-            style: new TextStyle(
-              fontFamily: 'Greycliff',
-              color: Colors.white,
-              fontSize: 25, 
-            )
-          ),
-          new Padding(
-            padding: const EdgeInsets.all(5.0)
-          ),
-          new Text(
-            datetime,
-            style: new TextStyle(
-              fontFamily: 'Roboto',
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w300, 
-            )
-          ),
-          new Padding(padding: const EdgeInsets.only(top: 3.0)),
-          new Text(
-            address,
-            style: new TextStyle(
-              fontFamily: 'Roboto',
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w300, 
-            )
-          ),
-          new Padding(
-            padding: const EdgeInsets.all(5.0)
-          ),
-          new RaisedButton(
-            onPressed: () { print("featured!"); },
-            color: Colors.white,
-            shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(5.0)
+          Hero(
+            tag: imgUrl,
+            child: ClipRRect(
+              borderRadius: new BorderRadius.all(const Radius.circular(0.0)),
+              child: new Image.network(
+                imgUrl,
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+              ),
             ),
+          ),
+          new Align(
+            alignment: Alignment.centerLeft,
             child: new Container(
-              width: 150.0,
-              height: 30.0,
-              alignment: Alignment.center,
-              child: new Row(
+              padding: const EdgeInsets.only(left:25.0),
+              child: new Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   new Text(
-                    'View Details',
-                    textAlign: TextAlign.center,
+                    title,
                     style: new TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Greycliff',
+                      color: Colors.white,
+                      fontSize: 40,
+                    )
+                  ),
+                  new Text(
+                    description,
+                    style: new TextStyle(
+                      fontFamily: 'Greycliff',
+                      color: Colors.white,
+                      fontSize: 25, 
+                    )
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0)
+                  ),
+                  new Text(
+                    datetime,
+                    style: new TextStyle(
+                      fontFamily: 'Roboto',
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300, 
+                    )
+                  ),
+                  new Padding(padding: const EdgeInsets.only(top: 3.0)),
+                  new Text(
+                    address,
+                    style: new TextStyle(
+                      fontFamily: 'Roboto',
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300, 
+                    )
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0)
+                  ),
+                  new RaisedButton(
+                    onPressed: () => _showEventDetail(),
+                    color: Colors.white,
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(5.0)
+                    ),
+                    child: new Container(
+                      width: 150.0,
+                      height: 30.0,
+                      alignment: Alignment.center,
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          new Text(
+                            'View Details',
+                            textAlign: TextAlign.center,
+                            style: new TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          new Icon(Icons.keyboard_arrow_right)
+                        ],
+                      ),
                     ),
                   ),
-                  new Icon(Icons.keyboard_arrow_right)
-                ],
+                ]
               ),
             ),
           ),
         ]
-      ),
+      )
     );
   }
 }
